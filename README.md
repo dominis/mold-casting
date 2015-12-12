8497c96ba102003d67f7c23ee5452273565a6784
Nandor Sivok <dominis@haxor.hu>
2015-12-12 15:58:42 +0100

    a

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-`curl -s https://raw.githubusercontent.com/dominis/master/boom.sh > /tmp/boom.sh ; bash /tmp/boom.sh`
+`bash "$(curl -fsSL https://raw.githubusercontent.com/dominis/master/boom.sh)"`
--- boom.sh
+++ boom.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 if [[ $(/usr/bin/fdesetup isactive) == "false" ]]
 then
   echo "failed, please enable full disk encryption (filevault) in system preferences"
@@ -14,10 +17,11 @@ sleep 10
 read -p "once it's installed press enter"
 sudo xcodebuild -license
 
-
 echo "install brew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "install brew cask"
 brew tap caskroom/cask
 
+echo "system update"
+sudo softwareupdate -i -a

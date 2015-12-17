23460bf1bbc5627e4db9c7457abd52a08ddf4db5
Nandor Sivok <dominis@users.noreply.github.com>
2015-12-17 17:42:23 +0100

    Update boom.sh

--- boom.sh
+++ boom.sh
@@ -28,7 +28,7 @@ fi
 mkdir -p ~/.ssh
 echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' >> ~/.ssh/known_hosts
 
-if [[ $(ssh -o BatchMode=yes git@github.com echo ok 2>&1) == "ok" ]]; then
+if [[ $(ssh -o BatchMode=yes git@github.com echo ok 2>&1) != "ok" ]]; then
   ssh-keygen -t rsa -b 4096 -C "dominis@haxor.hu"
   pbcopy < ~/.ssh/id_rsa.pub
   echo "new public key copied to your clipboard"
@@ -107,5 +107,6 @@ brew tap caskroom/versions
 echo "please setup dropbox"
 read -p "Press enter to continue"
 
+sudo easy_install pip
 sudo pip install mackup
 mackup restore
